159476c262ca3aeed9192e25340cd1521af8ca32
Christian Kellner <christian@kellner.me>
2015-01-13 17:17:17 +0100

    Functional: add Result<T> enum

--- SurveyApp/Functional.swift
+++ SurveyApp/Functional.swift
@@ -27,3 +27,38 @@ extension Array {
         return self.map{ closure(obj: $0) }.filter{ $0 != nil }.map{ $0! }
     }
 }
+
+final class BoxedValue<T> {
+    let val: T
+
+    init(_ value: T) {
+        val = value
+    }
+}
+
+enum Result<T> {
+    case Success(BoxedValue<T>)
+    case Error(NSError)
+
+    func map<U>(closure: (val: T) -> (U)) -> Result<U> {
+        switch self {
+        case .Success(let value):
+            return Result<U>.Success(BoxedValue<U>(closure(val: value.val)))
+        case .Error(let error):
+            return Result<U>.Error(error)
+        }
+    }
+
+    func flatMap<U>(closure: (val: T) -> (Result<U>)) -> Result<U> {
+        switch self {
+        case .Success(let value):
+            return closure(val: value.val)
+        case .Error(let error):
+            return Result<U>.Error(error)
+        }
+    }
+
+    static func make(value: T) -> Result<T> {
+        return Result<T>.Success(BoxedValue<T>(value))
+    }
+}
