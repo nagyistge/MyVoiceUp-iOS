0d5f4efa20bde0c3a771673943952cdf4ead056f
Christian Kellner <christian@kellner.me>
2015-04-20 16:25:25 +0200

    Add ResearchKit to swift ⥄ obj-c briding header

--- SurveyApp/SurveyApp-Bridging-Header.h
+++ SurveyApp/SurveyApp-Bridging-Header.h
@@ -2,9 +2,16 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#ifndef SA_BRIDGING_HEADER
+#define SA_BRIDGING_HEADER
+
+//Local stuff
 #import "Ohmage2Client.h"
 #import "DataHubClient.h"
 
+//Frameworks via CocoaPods
 #import <SVProgressHUD/SVProgressHUD.h>
-
 #import <CommonCrypto/CommonCrypto.h>
+#import <ResearchKit/ResearchKit.h>
+
+#endif //SA_BRIDGING_HEADER
